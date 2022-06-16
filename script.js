9d53c0cedaf8afe5292f6d79f7ad79d29ffed06d
wrainf <amirfaiz1806@gmail.com>
2022-06-16 11:04:41 +0800

    added operate funciton

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ function multiply(num1,num2){
 function divide(num1,num2){
     return num1/num2;
 }
+
+function operate(num1,num2,operator){
+    if(operator == '+'){
+        add(num1,num2);
+    }else if(operator == '-'){
+        subtract(num1,num2);
+    }else if(operator == '*'){
+        multiply(num1,num2);
+    }else{
+        divide(num1,num2);
+    }
+}
