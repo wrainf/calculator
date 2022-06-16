a21e5b0b7cff9802c570cedb29c55e747dbf313d
wrainf <amirfaiz1806@gmail.com>
2022-06-16 12:15:45 +0800

    completed basic functional calc

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const numberButtons = document.querySelectorAll('#number');
+const operatorButtons = document.querySelectorAll('#operator');
 const display = document.querySelector('#display');
+const equals = document.querySelector('#equals');
+const clear = document.querySelector('#clear');
+
+let number;
+let num1;
+let num2;
+let currOperator;
 
 numberButtons.forEach( (button) => {
     button.addEventListener('click',() =>{
@@ -7,16 +15,44 @@ numberButtons.forEach( (button) => {
     })
 });
 
+operatorButtons.forEach( (operator) => {
+    operator.addEventListener('click', () =>{
+        num1 = number;
+        clearDisplay();
+        currOperator = operator.value;
+    });
+});
+
+equals.addEventListener('click',()=>{
+    num2 = number;
+    let res = operate(num1,num2,currOperator);
+    clearDisplay();
+    populateDisplay(res)
+    number = res;
+});
+
+clear.addEventListener('click',()=>{
+    clearDisplay();
+})
+
+function clearDisplay(){
+    display.textContent = "";
+    number = '';
+    currOperator = '';
+}
 
 function populateDisplay(value){
     
     let text = display.textContent;
     if(value == 0 && text == ''){
         // do nothing as we do not want long strings of 0 at the start
-    }else{
-        text += value.toString();
-        display.textContent = text; 
     }
+    else{
+        text += value;
+        display.textContent = text; 
+        number = +text;
+    }
+    
     
 }
 
@@ -38,12 +74,12 @@ function divide(num1,num2){
 
 function operate(num1,num2,operator){
     if(operator == '+'){
-        add(num1,num2);
+        return add(num1,num2);
     }else if(operator == '-'){
-        subtract(num1,num2);
+        return subtract(num1,num2);
     }else if(operator == '*'){
-        multiply(num1,num2);
+        return multiply(num1,num2);
     }else{
-        divide(num1,num2);
+        return divide(num1,num2);
     }
 }
